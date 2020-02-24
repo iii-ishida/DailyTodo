277896ef81f77ca45d1d7c27fccee252c59a5c4f
iii-ishida <dev.j.ishida@gmail.com>
2020-02-24 14:24:41 +0900

    initial commit

---
+++ DailyTodo/RootViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RootViewController.swift
+//  DailyTodo
+//
+//  Created by ishida on 2020/02/24.
+//  Copyright © 2020 ishida. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+}
