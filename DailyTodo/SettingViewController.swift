9040f77d8e0928fcb69edd04736b1b48080a6192
iii-ishida <dev.j.ishida@gmail.com>
2020-02-26 22:30:48 +0900

    feat(ios-client): 設定画面＆ログアウト機能を追加

---
+++ DailyTodo/SettingViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SettingViewController.swift
+//  DailyTodo
+//
+//  Created by ishida on 2020/02/26.
+//  Copyright © 2020 ishida. All rights reserved.
+//
+
+import DailyTodoSDK
+import UIKit
+
+class SettingViewController: UIViewController {
+
+  @IBAction func onSignOut(_ sender: UIButton) {
+    do {
+      try DailyTodoSDK.signOut()
+    } catch {
+      let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+      alert.addAction(.ok)
+      self.present(alert, animated: true, completion: nil)
+    }
+  }
+}
