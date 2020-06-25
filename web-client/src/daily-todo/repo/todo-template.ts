31ab706f15a84f97367c0199f49fb99ca013298f
iii-ishida <dev.j.ishida@gmail.com>
2020-06-25 21:54:59 +0900

    feat(web-client): TodoTemplate の追加・更新処理を追加

--- web-client/src/daily-todo/repo/todo-template.ts
+++ web-client/src/daily-todo/repo/todo-template.ts
@@ -8,7 +8,33 @@ import { TodoTemplate, todoTemplateFromFirestoreDocument } from 'src/daily-todo/
 const db = firebase.firestore()
 
 export function watchTodoTemplateList(userId: string): Observable<TodoTemplate[]> {
-  return collectionData(todoTemplateCollection(userId), 'id').pipe(map((data) => data.map(todoTemplateFromFirestoreDocument)))
+  return collectionData(todoTemplateCollection(userId).orderBy('order'), 'id').pipe(map((data) => data.map(todoTemplateFromFirestoreDocument)))
+}
+
+export async function nextOrder(userId: string): Promise<number> {
+  const querySnapshot = await todoTemplateCollection(userId).orderBy('order', 'desc').limit(1).get()
+  const data = querySnapshot.docs[0]?.data()
+  return (data?.order ?? 0) + 1
+}
+
+export function addTodoTemplate(userId: string, todoTemplate: TodoTemplate): Promise<any> {
+  const { title, order } = todoTemplate
+
+  return todoTemplateCollection(userId).add({
+    title,
+    order,
+    done: false,
+    updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
+  })
+}
+
+export function updateTodoTemplate(userId: string, todoTemplate: TodoTemplate): Promise<any> {
+  const { title } = todoTemplate
+
+  return todoTemplateCollection(userId).add({
+    title,
+    updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
+  })
 }
 
 export function todoTemplateCollection(userId: string): firebase.firestore.CollectionReference {
