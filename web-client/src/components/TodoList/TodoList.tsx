121538e2ecb4f0b30bdc5f64a4ba55ce2d89ee2c
iii-ishida <dev.j.ishida@gmail.com>
2020-07-13 21:37:21 +0900

    feat(web-client): TodoList のデザインを変更

--- web-client/src/components/TodoList/TodoList.tsx
+++ web-client/src/components/TodoList/TodoList.tsx
@@ -9,6 +9,7 @@ const TodoListContainer: React.FC = () => {
   const dispatch = useDispatch()
   const userId = useSelector((state) => state.user?.id)
   const todoList = useSelector((state) => state.todoList)
+  const date = new Date()
 
   useEffect(() => {
     if (!userId) {
@@ -23,27 +24,41 @@ const TodoListContainer: React.FC = () => {
       return
     }
 
-    const subscription = todoRepo.watchTodoList(userId, new Date()).subscribe((list) => dispatch(TodoActions.recieve(list)))
+    const subscription = todoRepo.watchTodoList(userId, date).subscribe((list) => dispatch(TodoActions.recieve(list)))
 
     return () => subscription.unsubscribe()
   }, [dispatch, userId])
 
-  return <TodoList userId={userId} todoList={todoList} />
+  return <TodoList userId={userId} date={date} todoList={todoList} />
 }
 
 type Props = {
   userId: string
+  date: Date
   todoList: Todo[]
 }
 
-const TodoList: React.FC<Props> = ({ userId, todoList }: Props) => (
-  <ul>
-    {todoList.map((todo) => (
-      <li key={todo.id} className={styles.todoListItemContainer}>
-        <TodoListItem userId={userId} todo={todo} />
-      </li>
-    ))}
-  </ul>
+const TodoList: React.FC<Props> = ({ userId, date, todoList }: Props) => (
+  <div>
+    <h1 className={styles.header}>{formatDate(date)}</h1>
+    <ul className={styles.todoList}>
+      {todoList.map((todo) => (
+        <li key={todo.id} className={styles.todoListItemContainer}>
+          <TodoListItem userId={userId} todo={todo} />
+        </li>
+      ))}
+    </ul>
+  </div>
 )
+
+function formatDate(date: Date): string {
+  const zeroPad = (x) => (x >= 10 ? '' + x : '0' + x)
+
+  const yyyy = date.getFullYear()
+  const mm = zeroPad(date.getMonth() + 1)
+  const dd = zeroPad(date.getDate())
+
+  return `${yyyy}年${mm}月${dd}日`
+}
 
 export default TodoListContainer
