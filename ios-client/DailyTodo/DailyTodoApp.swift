8da7881678f50275d50dde3fce0d4c1bb460a9a6
iii-ishida <dev.j.ishida@gmail.com>
2020-09-21 15:37:49 +0900

    feat(ios-client): SwiftUI で作り直し

---
+++ ios-client/DailyTodo/DailyTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyTodoApp.swift
+//  DailyTodo
+//
+//  Created by ishida on 2020/09/21.
+//
+
+import SwiftUI
+
+@main
+struct DailyTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
